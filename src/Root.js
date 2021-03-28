491a8e89b04fa4cd9148d7a815932c87f7268489
Donquixote Sabo <mixboksu@gmail.com>
2021-03-28 12:03:52 +0200

    Project config

---
+++ src/Root.js
@@ -0,0 +1,9 @@
+function Root() {
+  return (
+    <div>
+      siema <h1>zaczynamy impreze</h1>
+    </div>
+  );
+}
+
+export default Root;
---
+++ src/assets/styles/theme.js
@@ -0,0 +1,30 @@
+const theme = {
+  colors: {
+    white: 'fff',
+    gray: '#3F3F3F',
+    silver: '9E9E9E',
+    //Button colors
+    default: '#E0E0E0',
+    defaultHover: '#AEAEAE',
+    primary: '#2962FF',
+    primaryHover: '#0039CB',
+    secondary: '#455A64',
+    secondaryHover: '#1C313A',
+    danger: '#D32F2F',
+    dangerHover: '#9A0007',
+  },
+  fontSize: {
+    xs: '12px',
+    s: '14px',
+    m: '16px',
+    l: '18px',
+    xl: '20px',
+  },
+  buttonSizes: {
+    sm: '5px 10px',
+    md: '10px 15px',
+    lg: '20px 30px',
+  },
+};
+
+export default theme;
