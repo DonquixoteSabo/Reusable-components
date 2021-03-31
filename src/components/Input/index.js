2489b9d139872373e656cbf19dc4f1c69dc33054
Donquixote Sabo <mixboksu@gmail.com>
2021-03-31 10:51:04 +0200

    feat: :sparkles: Add fullWidth and size props

--- src/components/Input/index.js
+++ src/components/Input/index.js
@@ -16,7 +16,7 @@ const Group = styled.div`
   }
   .container {
     position: relative;
-    width: 200px;
+    width: ${props => (props.fullWidth ? '100%' : '200px')};
     height: 56px;
     display: flex;
     align-items: center;
@@ -29,8 +29,16 @@ const Group = styled.div`
         right: 5px;
       }
     }
+    /*SIZE */
+    ${props =>
+      props.size === 'sm' &&
+      css`
+        width: 200px;
+        height: 40px;
+      `}
   }
-  /*Error*/
+
+  /*ERROR*/
   ${props =>
     props.error &&
     css`
@@ -86,45 +94,18 @@ const StyledInput = styled.input`
 `;
 
 function Input(props) {
-  if (props.startIcon) {
-    return (
-      <Group error={props.error}>
-        <label className='color' htmlFor='input'>
-          {props.label}
-        </label>
-
-        <div className='container'>
-          <span className='material-icons start'>{props.startIcon}</span>
-          <StyledInput id='input' name='input' type='text' {...props} />
-        </div>
-
-        <small className='color'>{props.helperText}</small>
-      </Group>
-    );
-  }
-  if (props.endIcon) {
-    return (
-      <Group error={props.error}>
-        <label className='color' htmlFor='input'>
-          {props.label}
-        </label>
-
-        <div className='container'>
-          <span className='material-icons end'>{props.endIcon}</span>
-          <StyledInput id='input' name='input' type='text' {...props} />
-        </div>
-
-        <small className='color'>{props.helperText}</small>
-      </Group>
-    );
-  }
   return (
-    <Group error={props.error}>
+    <Group error={props.error} size={props.size} fullWidth={props.fullWidth}>
       <label className='color' htmlFor='input'>
         {props.label}
       </label>
 
       <div className='container'>
+        {props.startIcon ? (
+          <span className='material-icons start'>{props.startIcon}</span>
+        ) : props.endIcon ? (
+          <span className='material-icons end'>{props.endIcon}</span>
+        ) : null}
         <StyledInput id='input' name='input' type='text' {...props} />
       </div>
 
