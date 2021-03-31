edcdf2cf1abbf8f0c56a3147f9822c8aac8af537
Donquixote Sabo <mixboksu@gmail.com>
2021-03-31 10:29:05 +0200

    feat: :sparkles: Add startIcon and endIcon props

--- src/components/Input/index.js
+++ src/components/Input/index.js
@@ -2,19 +2,35 @@ import React from 'react';
 import styled, { css } from 'styled-components';
 
 const Group = styled.div`
-  color: ${({ theme }) => theme.colors.gray3};
   display: flex;
   flex-direction: column;
-  label {
-    font-weight: normal;
-    font-size: ${({ theme }) => theme.fontSize.xs};
+  color: ${({ theme }) => theme.colors.gray3};
+  font-size: ${({ theme }) => theme.fontSize.xs};
+  small {
+    font-size: 10px;
   }
   &:focus-within {
     .color {
       color: ${({ theme }) => theme.colors.primary};
     }
   }
-
+  .container {
+    position: relative;
+    width: 200px;
+    height: 56px;
+    display: flex;
+    align-items: center;
+    span {
+      position: absolute;
+      &.start {
+        left: 5px;
+      }
+      &.end {
+        right: 5px;
+      }
+    }
+  }
+  /*Error*/
   ${props =>
     props.error &&
     css`
@@ -27,8 +43,8 @@ const Group = styled.div`
 `;
 
 const StyledInput = styled.input`
-  width: 200px;
-  height: 56px;
+  width: 100%;
+  height: 100%;
   border-radius: 8px;
   padding: 10px;
   border: 1px solid ${({ theme }) => theme.colors.gray3};
@@ -53,28 +69,66 @@ const StyledInput = styled.input`
     `}
 
   ${props =>
-    props.error &&
+    props.disabled &&
     css`
       background: #f2f2f2;
-
       border: 1px solid ${({ theme }) => theme.colors.default};
+      &:hover {
+        border: 1px solid ${({ theme }) => theme.colors.default};
+      }
     `}
-`;
 
-const HelperText = styled.small`
-  font-weight: normal;
-  font-size: 10px;
-  color: ${({ theme }) => theme.colors.gray3};
+    ${props =>
+    (props.startIcon || props.endIcon) &&
+    css`
+      text-indent: 20px;
+    `};
 `;
 
 function Input(props) {
+  if (props.startIcon) {
+    return (
+      <Group error={props.error}>
+        <label className='color' htmlFor='input'>
+          {props.label}
+        </label>
+
+        <div className='container'>
+          <span className='material-icons start'>{props.startIcon}</span>
+          <StyledInput id='input' name='input' type='text' {...props} />
+        </div>
+
+        <small className='color'>{props.helperText}</small>
+      </Group>
+    );
+  }
+  if (props.endIcon) {
+    return (
+      <Group error={props.error}>
+        <label className='color' htmlFor='input'>
+          {props.label}
+        </label>
+
+        <div className='container'>
+          <span className='material-icons end'>{props.endIcon}</span>
+          <StyledInput id='input' name='input' type='text' {...props} />
+        </div>
+
+        <small className='color'>{props.helperText}</small>
+      </Group>
+    );
+  }
   return (
     <Group error={props.error}>
       <label className='color' htmlFor='input'>
         {props.label}
       </label>
-      <StyledInput id='input' name='input' type='text' {...props} />
-      <HelperText className='color'>{props.helperText}</HelperText>
+
+      <div className='container'>
+        <StyledInput id='input' name='input' type='text' {...props} />
+      </div>
+
+      <small className='color'>{props.helperText}</small>
     </Group>
   );
 }
